b9fe132120358e4009f40720ba9b66820b4fc0b9
APGLE <thefirefirst@gmail.com>
2023-10-05 10:44:25 +0200

    Tercera modificacion

--- modelos/src/modelos/Interfaz_Menu.java
+++ modelos/src/modelos/Interfaz_Menu.java
@@ -1,12 +1,42 @@
 package modelos;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
 
 public class Interfaz_Menu extends JFrame {
+	public Interfaz_Menu() {
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 10, 436, 44);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(10, 10, 33, 21);
+		panel_1.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(109, 11, 190, 19);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(322, 14, 86, 13);
+		panel_1.add(lblNewLabel);
+	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private JTextField textField;
 }
