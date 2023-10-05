21904c9b61629a0760d6c811f381c605802d06bc
Jose Perez <josepemar@gmail.com>
2023-10-05 23:44:54 +0800

    Commit 1 Jose
    Añadir tres clases nuevas. Y amplicacion de la clase modelo

---
+++ Trabajo_Acceso a datos/src/modelos/Modelo.java
@@ -0,0 +1,114 @@
+package modelos;
+
+import modelos.Controlador;
+
+public class Modelo {
+	private Controlador controlador;
+
+	public Modelo(Controlador controlador) {
+		super();
+
+		this.controlador = controlador;
+	}
+
+	public String registrarProducto() {
+
+		return "";
+	}
+
+	public String actualizarProducto() {
+
+		return "";
+	}
+
+	public String eliminarProducto() {
+
+		return "";
+	}
+
+	public String listarProducto() {
+
+		return "";
+	}
+
+	public String insertImg() {
+
+		return "";
+	}
+
+	public String eliminarImg() {
+
+		return "";
+	}
+
+	public String cambiarImg() {
+
+		return "";
+	}
+
+	public String mostrarImg() {
+
+		return "";
+	}
+
+	public String registrarProveedor() {
+
+		return "";
+	}
+
+	public String actualizarProveedor() {
+
+		return "";
+	}
+
+	public String eliminarProveedor() {
+
+		return "";
+	}
+
+	public String listarProveedor() {
+
+		return "";
+	}
+
+	public String recibirProductoProveedor() {
+
+		return "";
+	}
+
+	public String ventaProducto() {
+
+		return "";
+	}
+
+	public String listaTransacciones() {
+
+		return "";
+	}
+
+	public String actualizarStock() {
+
+		return "";
+	}
+
+	public String buscarProducto() {
+
+		return "";
+	}
+
+	public String informesInventario() {
+
+		return "";
+	}
+
+	public String registrarUsuario() {
+
+		return "";
+	}
+
+	public String iniciarSesion() {
+
+		return "";
+	}
+
+}
