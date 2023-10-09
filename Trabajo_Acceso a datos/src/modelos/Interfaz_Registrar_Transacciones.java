887bc1738c9a9a9b00f8495375183492efa513ff
APGLE <thefirefirst@gmail.com>
2023-10-09 13:16:32 +0200

    Update Interfaz_Registrar_Transacciones.java

--- Trabajo_Acceso a datos/src/modelos/Interfaz_Registrar_Transacciones.java
+++ Trabajo_Acceso a datos/src/modelos/Interfaz_Registrar_Transacciones.java
@@ -1,12 +1,30 @@
 package modelos;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTable;
+import javax.swing.JButton;
 
 public class Interfaz_Registrar_Transacciones extends JFrame {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	public Interfaz_Registrar_Transacciones() {
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(10, 10, 318, 243);
+		panel.add(table);
+		
+		JButton btnNewButton = new JButton("Refrescar");
+		btnNewButton.setBounds(338, 217, 85, 21);
+		panel.add(btnNewButton);
+	}
+	private JTable table;
 }
