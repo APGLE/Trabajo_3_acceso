6adfcfe75280839acfc520e83d6c3199e52055df
APGLE <thefirefirst@gmail.com>
2023-10-09 12:49:59 +0200

    Actualizacion de objeto

--- Trabajo_Acceso a datos/src/modelos/Interfaz_Objeto.java
+++ Trabajo_Acceso a datos/src/modelos/Interfaz_Objeto.java
@@ -1,42 +1,78 @@
 package modelos;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Interfaz_Objeto extends JFrame {
-	public Interfaz_Objeto() {
-		
-		JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setBounds(10, 10, 434, 44);
-		panel.add(panel_1);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(10, 10, 33, 21);
-		panel_1.add(btnNewButton);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(109, 11, 190, 19);
-		panel_1.add(textField);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(322, 14, 86, 13);
-		panel_1.add(lblNewLabel);
-	}
-
-	/**
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
 
+	public Interfaz_Objeto(String nombre, String descripcion, double precio, String categoria, String imagenPath) {
+        // Configurar la ventana de detalles del objeto
+        setTitle("Detalles del Objeto");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Crear un panel para el contenido
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BorderLayout());
+
+        // Panel para la imagen
+        JPanel imagenPanel = new JPanel();
+        imagenPanel.setPreferredSize(new Dimension(200, 200));
+        imagenPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        // Cargar la imagen del objeto
+        JLabel imagenLabel = new JLabel();
+        try {
+            BufferedImage imagen = ImageIO.read(new File(imagenPath));
+            ImageIcon imagenIcon = new ImageIcon(imagen.getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+            imagenLabel.setIcon(imagenIcon);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imagenPanel.add(imagenLabel);
+
+        // Panel para la descripción, nombre y categoría
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BorderLayout());
+
+        JLabel nombreLabel = new JLabel("Nombre: " + nombre);
+        JLabel precioLabel = new JLabel("Precio: " + precio);
+        JLabel descripcionLabel = new JLabel("Descripción: " + descripcion);
+        JLabel categoriaLabel = new JLabel("Categoría: " + categoria);
+
+        infoPanel.add(nombreLabel, BorderLayout.NORTH);
+        infoPanel.add(precioLabel, BorderLayout.EAST);
+        infoPanel.add(descripcionLabel, BorderLayout.CENTER);
+        infoPanel.add(categoriaLabel, BorderLayout.SOUTH);
+
+        // Agregar elementos al panel de contenido
+        contentPanel.add(imagenPanel, BorderLayout.WEST);
+        contentPanel.add(infoPanel, BorderLayout.CENTER);
+
+        // Agregar el panel de contenido a la ventana
+        getContentPane().add(contentPanel);
+
+        // Mostrar la ventana
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // Ejemplo de uso de la interfaz de objeto
+                new Interfaz_Objeto("Producto 1", "Descripción del producto 1", 29.99, "Electrónica", "imagen_producto1.jpg");
+            }
+        });
+    }
 }
