d293b612846b734f40b6ab40b8cab8f918c5edba
APGLE <thefirefirst@gmail.com>
2023-10-10 13:13:37 +0200

    Actualizar objetos registrar

--- Trabajo_Acceso a datos/src/vista/Interfaz_Registrar_Objetos.java
+++ Trabajo_Acceso a datos/src/vista/Interfaz_Registrar_Objetos.java
@@ -1,21 +1,18 @@
 package vista;
 
-//package modelos;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Interfaz_Registrar_Objetos extends JFrame {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public Interfaz_Registrar_Objetos() {
+    public Interfaz_Registrar_Objetos() {
         // Configurar la ventana de agregar producto
         setTitle("Agregar Producto");
         setSize(400, 400);
@@ -30,10 +27,15 @@ public class Interfaz_Registrar_Objetos extends JFrame {
         JPanel formularioPanel = new JPanel();
 
         // Campos de entrada
+        JLabel lblId = new JLabel("ID:");
+        lblId.setBounds(10, 9, 46, 25);
+        JTextField txtId = new JTextField();
+        txtId.setBounds(89, 5, 188, 33);
+
         JLabel lblNombre = new JLabel("Nombre:");
-        lblNombre.setBounds(10, 35, 46, 25);
+        lblNombre.setBounds(10, 52, 46, 25);
         JTextField txtNombre = new JTextField();
-        txtNombre.setBounds(89, 31, 188, 33);
+        txtNombre.setBounds(89, 48, 188, 33);
 
         JLabel lblDescripcion = new JLabel("Descripción:");
         lblDescripcion.setBounds(10, 91, 66, 25);
@@ -54,14 +56,13 @@ public class Interfaz_Registrar_Objetos extends JFrame {
 
         JLabel lblImagen = new JLabel("Imagen:");
         lblImagen.setBounds(10, 281, 46, 31);
-        JButton btnCargarImagen = new JButton("Cargar Imagen");
-        btnCargarImagen.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        	}
-        });
-        btnCargarImagen.setBounds(89, 281, 188, 31);
+        JTextField txtImagen = new JTextField();
+        txtImagen.setBounds(89, 281, 188, 31);
+
         formularioPanel.setLayout(null);
 
+        formularioPanel.add(lblId);
+        formularioPanel.add(txtId);
         formularioPanel.add(lblNombre);
         formularioPanel.add(txtNombre);
         formularioPanel.add(lblDescripcion);
@@ -71,21 +72,33 @@ public class Interfaz_Registrar_Objetos extends JFrame {
         formularioPanel.add(lblCategoria);
         formularioPanel.add(cboCategoria);
         formularioPanel.add(lblImagen);
-        formularioPanel.add(btnCargarImagen);
+        formularioPanel.add(txtImagen);
 
         // Botón para agregar el producto
         JButton btnAgregar = new JButton("Agregar Producto");
         btnAgregar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Obtener los valores del producto
+                // Obtener los valores del producto desde los campos de entrada
+                String id = txtId.getText();
                 String nombre = txtNombre.getText();
                 String descripcion = txtDescripcion.getText();
                 double precio = Double.parseDouble(txtPrecio.getText());
                 String categoria = (String) cboCategoria.getSelectedItem();
+                String imagen = txtImagen.getText(); // Obtener la ruta de la imagen
 
-                // Lógica para agregar el producto a tu sistema
-                agregarProducto(nombre, descripcion, precio, categoria);
+                // Lógica para agregar el producto a la base de datos
+                if (guardarProductoEnBaseDeDatos(id, nombre, descripcion, precio, categoria, imagen)) {
+                    JOptionPane.showMessageDialog(
+                        Interfaz_Registrar_Objetos.this,
+                        "Producto agregado con éxito."
+                    );
+                } else {
+                    JOptionPane.showMessageDialog(
+                        Interfaz_Registrar_Objetos.this,
+                        "Error al agregar el producto. Verifica la conexión a la base de datos."
+                    );
+                }
             }
         });
 
@@ -100,11 +113,34 @@ public class Interfaz_Registrar_Objetos extends JFrame {
         setVisible(true);
     }
 
-    private void agregarProducto(String nombre, String descripcion, double precio, String categoria) {
-        // Lógica para agregar el producto a tu sistema
-        // Debes implementar la lógica real para almacenar los datos del producto aquí
-        JOptionPane.showMessageDialog(this, "Producto agregado:\nNombre: " + nombre + "\nDescripción: " + descripcion
-                + "\nPrecio Unitario: " + precio + "\nCategoría: " + categoria);
+    // Función para guardar el producto en la base de datos
+    private boolean guardarProductoEnBaseDeDatos(String id, String nombre, String descripcion, double precio, String categoria, String imagen) {
+        String url = "jdbc:mysql://localhost:3306/tiendamuebles";
+        String usuario = "tu_usuario";
+        String contraseña = "tu_contraseña";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, usuario, contraseña);
+            String consulta = "INSERT INTO producto (id, nombre, descripcion, precio, categoria, imagen) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(consulta);
+            preparedStatement.setString(1, id);
+            preparedStatement.setString(2, nombre);
+            preparedStatement.setString(3, descripcion);
+            preparedStatement.setDouble(4, precio);
+            preparedStatement.setString(5, categoria);
+            preparedStatement.setString(6, imagen); // Insertar la ruta de la imagen
+
+            int filasAfectadas = preparedStatement.executeUpdate();
+
+            // Cerrar la conexión
+            preparedStatement.close();
+            connection.close();
+
+            return filasAfectadas > 0; // Retorna true si al menos una fila fue afectada (éxito)
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false; // Retorna false en caso de error
+        }
     }
 
     public static void main(String[] args) {
